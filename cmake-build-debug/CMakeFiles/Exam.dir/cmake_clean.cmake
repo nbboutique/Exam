0668045298f5e519e276e1e847a70e38177edfec
nbboutique <46777706+nbboutique@users.noreply.github.com>
2021-12-20 11:10:07 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/Exam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exam.dir/main.cpp.o"
+  "Exam"
+  "Exam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
